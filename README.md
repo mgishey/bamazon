a3877ab1867bcf1665441bbf6a0df1eccf6bab67
Mike Gishey <mgishey56@gmail.com>
2019-10-18 20:39:48 -0700

    finished bamazon Manager

--- README.md
+++ README.md
@@ -1,17 +1,42 @@
-# bamazon
+# bamazonCustomer.js
 A CLI app using MySQL and node.js to create an application similar to the type used behind the counter at auto parts stores.  In this case we are dealing with motorcycles and their accessories.
 
 ## Step by Step Instructions
 1. Open a terminal
 2. Type the command `node bamazonCustomer.js` at prompt to start the application.
 
-    **Example 1:** In this example the user requests more units of a product than there are in inventory.  The user is notified than there is not enough product and the application ends
+    **Example 1:** In this example the user requests more units of a product than there are in inventory.  The user is notified than there is not enough product and the application ends.
 
     ![bam-not-in-stock](/images/bam-not-in-stock.png)
 
     **Example 2** Here the user requests a product in which inventory is sufficent to handle the number of units requested. The existing stock is depleted by the number of units the customer requested.  The database is updated to reflect this change in stock. The customer is then presented with the total price.
 
     ![bam-in-stock](/images/bam-in-stock.png)
+
+# bamazonManager.js
+Another CLI app that lets a manager of the store:
+* View products for sale.
+* View low inventory (anything under 5 units).
+* Add to inventory.
+* Add a new product.
+
+## Step by Step Instructions
+1. Open a terminal.
+2. Type the command `node bamazonManager.js`.
+    **Example 1:** In this example the manager selects the menu option `View Products For Sale` and is presented with the items in the products table.
+
+    **Example 2:** After displaying the products, the manager uses the `View Low Inventory` menu option and is presenting with a listing of all products with under 5 units in stock
+
+    ![bam-mgr-1](/images/bam-mgr-2.png)
+
+    **Example 3:** 
+
+    ![bam-mgr-2](/images/bam-mgr-2.png)
+
+    **Example 4:**
+
+    ![bam-mgr-3](/images/bam-mgr-3.png)
+
 
 ## Tecnologies Used
 
--- bamazonManager.js
+++ bamazonManager.js
@@ -22,17 +22,109 @@ db.connect(function (err) {
     start();
 });
 
-function start(){
+function start() {
     inquirer
-    .prompt([
-        {
-            type: 'list',
-            name: 'viewOption',
-            message: 'Select Menu Option',
-            choices: ['View Products For Sale', 'View Low Inventory', 'Add to Inventory', 'Add New Produt'],
+        .prompt([
+            {
+                type: 'list',
+                name: 'viewOption',
+                message: 'Select Menu Option',
+                choices: ['View Products For Sale', 'View Low Inventory', 'Add to Inventory', 'Add New Product', 'Quit'],
 
-        },
-    ]).then(answers => {
-        console.log('Answer ' + answers.viewOption);
+            },
+        ]).then(answers => {
+            switch (answers.viewOption) {
+                case 'View Products For Sale':
+                    forSale();
+                    break;
+                case 'View Low Inventory':
+                    lowInventory();
+                    break;
+                case 'Add to Inventory':
+                    addInventory();
+                    break;
+                case 'Add New Product':
+                    addNew();
+                    break;
+                case 'Quit':
+                    db.end();
+                default:
+                    forSale();
+            }
+        });
+}
+
+function forSale() {
+    console.log("FOR SALE!!!");
+    let sql = "SELECT item_id, product_name, price, stock_quantity FROM products";
+    db.query(sql, function (err, res) {
+        if (err) throw err;
+        console.table(res);
+        db.end();
     });
+}
+
+function lowInventory() {
+    let sql = "SELECT item_id, product_name, price, stock_quantity FROM products WHERE stock_quantity < 5";
+    db.query(sql, function (err, res) {
+        if (err) throw err;
+        console.table(res);
+        db.end();
+    });
+}
+
+function addInventory() {
+    inquirer
+        .prompt([
+            {
+                name: 'itemID',
+                message: "Select product to add inventory by ID"
+            },
+            {
+                name: 'quantity',
+                message: 'How many would you like to add'
+            }
+        ]).then(ans => {
+            let sql = "SELECT stock_quantity FROM products WHERE item_id = " + ans.itemID;
+            db.query(sql, function (err, res) {
+                if (err) throw err;
+                let newQuantity = parseInt(res[0].stock_quantity) + parseInt(ans.quantity);
+                let sql1 = "UPDATE products SET stock_quantity = " + newQuantity + " WHERE item_id = " + ans.itemID;
+                db.query(sql1, function (err, resp) {
+                    if (err) throw err;
+                    //console.log(resp);
+                    db.end();
+                });
+            });
+        });
+}
+
+function addNew() {
+    inquirer
+        .prompt([
+            {
+                name: 'product',
+                message: 'Enter product name:'
+            },
+            {
+                name: 'dept',
+                message: 'Enter a department:'
+            },
+            {
+                name: 'price',
+                message: 'Enter price:'
+            },
+            {
+                name: 'quantity',
+                message: 'Enter number of units:'
+            }
+        ]).then(ans => {
+            let sql = "INSERT INTO products (product_name, department_name, price, stock_quantity) VALUES ('" + ans.product + "', '" + ans.dept + "', '" + ans.price + "', '" + ans.quantity + "')";
+            //console.log(sql);
+            db.query(sql, function (err, resp) {
+                if (err) throw err;
+                console.log(ans.quantity + " " + ans.product + "s added to inventory");
+                db.end();
+            });
+        });  
 }
